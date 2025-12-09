eb975dbf2e430dfd9ca222180e80ed6dbfe6f608
dehb <teejaysunimi@gmail.com>
2025-12-09 21:17:17 +0100

    Project set up

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
+
+// https://www.figma.com/community/file/1117253657372214910
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar
+      // barStyle="dark-content"
+      />
+      <AppNavigator />
+    </>
   );
+
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Open up App.tsx to start working on your app!</Text>
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
